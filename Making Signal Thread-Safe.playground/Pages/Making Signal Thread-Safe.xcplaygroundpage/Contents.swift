cca8e7968071c531b6f1094f0b687a2bd8b6fb33
Florian Kugler <mail@floriankugler.com>
2017-02-14 12:35:21 +0100

    Initial commit

---
+++ Making Signal Thread-Safe.playground/Pages/Making Signal Thread-Safe.xcplaygroundpage/Contents.swift
@@ -0,0 +1,147 @@
+import Cocoa
+
+enum Result<A> {
+    case success(A)
+    case error(Error)
+    
+    init(_ value: A?, or error: Error) {
+        if let value = value {
+            self = .success(value)
+        } else {
+            self = .error(error)
+        }
+    }
+}
+
+final class KeyValueObserver<A>: NSObject {
+    let block: (A) -> ()
+    let keyPath: String
+    var object: NSObject
+    init(object: NSObject, keyPath: String, _ block: @escaping (A) -> ()) {
+        self.block = block
+        self.keyPath = keyPath
+        self.object = object
+        super.init()
+        object.addObserver(self, forKeyPath: keyPath, options: .new, context: nil)
+    }
+    
+    deinit {
+        object.removeObserver(self, forKeyPath: keyPath)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        block(change![.newKey] as! A)
+    }
+}
+
+
+extension Result {
+    func map<B>(_ transform: (A) -> B) -> Result<B> {
+        switch self {
+        case .success(let value): return .success(transform(value))
+        case .error(let error): return .error(error)
+        }
+    }
+}
+
+extension String: Error { }
+
+final class Atomic<A> {
+    private var queue = DispatchQueue(label: "serial queue")
+    private var _value: A
+    init(_ value: A) {
+        self._value = value
+    }
+    
+    var value: A {
+        get {
+            return queue.sync { self._value }
+        }
+        set {
+            queue.sync { self._value = newValue }
+        }
+    }
+}
+
+final class Signal<A> {
+    private typealias Token = UUID
+    private typealias Callbacks = [Token:(Result<A>) -> ()]
+    private var callbacks: Atomic<Callbacks> = Atomic([:])
+    private var objects: Atomic<[Any]> = Atomic([])
+    
+    static func pipe() -> ((Result<A>) -> (), Signal<A>) {
+        let signal = Signal<A>()
+        return ({ [weak signal] value in signal?.send(value) }, signal)
+    }
+    
+    private func send(_ value: Result<A>) {
+        for callback in callbacks.value.values {
+            callback(value)
+        }
+    }
+    
+    func subscribe(callback: @escaping (Result<A>) -> ()) -> Disposable {
+        let token = UUID()
+        self.callbacks.value[token] = callback
+        return Disposable {
+            self.callbacks.value[token] = nil
+        }
+    }
+    
+    func keepAlive(_ object: Any) {
+        objects.value.append(object)
+    }
+    
+    func map<B>(_ transform: @escaping (A) -> B) -> Signal<B> {
+        let (sink, result) = Signal<B>.pipe()
+        let disposable = subscribe { value in
+            sink(value.map(transform))
+        }
+        result.keepAlive(disposable)
+        return result
+    }
+    
+    deinit {
+        print("deiniting signal")
+    }
+}
+
+
+final class Disposable {
+    let dispose: () -> ()
+    init(_ dispose: @escaping () -> ()) {
+        self.dispose = dispose
+    }
+    deinit {
+        dispose()
+    }
+}
+
+extension NSTextField {
+    func signal() -> Signal<String> {
+        let (sink, result) = Signal<String>.pipe()
+        let observer = KeyValueObserver(object: self, keyPath: #keyPath(stringValue)) { str in
+            sink(.success(str))
+        }
+        result.keepAlive(observer)
+        return result
+    }
+}
+
+class VC {
+    let textField = NSTextField()
+    var disposables: [Disposable] = []
+    
+    func viewDidLoad() {
+        let intSignal = textField.signal().map { Int($0) }
+        let disposable = intSignal.subscribe {
+            print($0)
+        }
+        disposables.append(disposable)
+    }
+}
+
+var vc: VC? = VC()
+vc?.viewDidLoad()
+vc?.textField.stringValue = "17"
+vc = nil
---
+++ README.md
@@ -0,0 +1,4 @@
+# Swift Talk
+## Thread-Safety (Reactive Programming #5)
+
+This is the code that accompanies Swift Talk Episode 42: [Thread-Safety (Reactive Programming #5) ](https://talk.objc.io/episodes/S01E42-thread-safety-reactive-programming-5)
